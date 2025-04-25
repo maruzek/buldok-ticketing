73a2f8167904b2c0f98ad79020bfb3654fb4662a
Martin Růžek <me@martinruzek.cz>
2025-04-25 21:06:33 +0200

    added api and login controller

---
+++ backend/src/Controller/ApiController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/api', name: 'api')]
+final class ApiController extends AbstractController
+{
+    #[Route('/', name: 'api_index')]
+    public function index(): JsonResponse
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/ApiController.php',
+        ]);
+    }
+}
