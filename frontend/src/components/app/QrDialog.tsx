622094f0a30cf6546c049dc2cb44260ecd6ac877
Martin Růžek <me@martinruzek.cz>
2025-09-02 23:04:53 +0200

    removed unused imports

--- frontend/src/components/app/QrDialog.tsx
+++ frontend/src/components/app/QrDialog.tsx
@@ -9,8 +9,6 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 import { QRCodeSVG } from "qrcode.react";
 
 type QrDialogProps = {
