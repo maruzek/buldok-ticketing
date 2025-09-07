1ee834b4dd2d07f9fbf86e4c4a5370386ffdf013
Martin Růžek <me@martinruzek.cz>
2025-09-07 17:17:31 +0200

    converted entrance list to shad

--- frontend/src/components/admin/EntranceList.tsx
+++ frontend/src/components/admin/EntranceList.tsx
@@ -5,6 +5,8 @@ import { Entrance } from "../../types/Entrance";
 import useApi from "../../hooks/useApi";
 import { EditStatus } from "../../types/EditStatus";
 import { useQuery } from "@tanstack/react-query";
+import { EntrancesDataTable } from "./EntranceTable/data-table";
+import { columns } from "./EntranceTable/columns";
 
 type EntranceListProps = {
   entranceStatus: EditStatus | null;
@@ -35,56 +37,59 @@ const EntranceList = ({ entranceStatus }: EntranceListProps) => {
   }
 
   return (
-    <div className=" w-full p-5 bg-white rounded-xl">
-      {error && <div className="form-error-box">{error}</div>}
-      {entranceStatus?.status == "ok" && (
-        <div className="form-success-box">{entranceStatus.message}</div>
-      )}
-      <div className="table-none md:table-fixed">
-        <table className="table-auto w-full border border-gray-200 rounded-lg overflow-hidden">
-          <thead className="bg-gray-100">
-            <tr>
-              <th className="px-4 py-2 text-left">ID</th>
-              <th className="px-4 py-2 text-left">Název</th>
-              <th className="px-4 py-2 text-left">Lokace</th>
-              <th className="px-4 py-2 text-left">Pokladníci</th>
-              <th className="px-4 py-2 text-left">Akce</th>
-            </tr>
-          </thead>
-          <tbody>
-            {entrances.map((ent) => (
-              <tr
-                key={ent.id}
-                className="border-t border-gray-200 hover:bg-gray-50"
-              >
-                <td className="px-4 py-2">{ent.id}</td>
-                <td className="px-4 py-2">{ent.name}</td>
-                <td className="px-4 py-2">{ent.location || "-"}</td>
-                <td>
-                  {ent.users &&
-                    ent.users.map((user, i) => {
-                      if (i > 0)
-                        return <span key={user.id}>, {user.email}</span>;
-                      return <span key={user.id}>{user.email}</span>;
-                    })}
-                </td>
-                <td className="px-4 py-2 flex items-center ">
-                  <Link
-                    to={`/admin/entrances/${ent.id}/edit`}
-                    className="bg-blue-500 hover:bg-blue-600 text-white py-1 px-3 rounded text-xs cursor-pointer"
-                  >
-                    <Settings className="w-5 h-5" />
-                  </Link>
-                  <button className="bg-red-500 hover:bg-red-600 text-white py-1 px-3 rounded text-xs cursor-pointer ml-2">
-                    <Trash2 className="w-5 h-5" />
-                  </button>
-                </td>
+    <>
+      {/* <div className=" w-full p-5 bg-white rounded-xl">
+        {error && <div className="form-error-box">{error}</div>}
+        {entranceStatus?.status == "ok" && (
+          <div className="form-success-box">{entranceStatus.message}</div>
+        )}
+        <div className="table-none md:table-fixed">
+          <table className="table-auto w-full border border-gray-200 rounded-lg overflow-hidden">
+            <thead className="bg-gray-100">
+              <tr>
+                <th className="px-4 py-2 text-left">ID</th>
+                <th className="px-4 py-2 text-left">Název</th>
+                <th className="px-4 py-2 text-left">Lokace</th>
+                <th className="px-4 py-2 text-left">Pokladníci</th>
+                <th className="px-4 py-2 text-left">Akce</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-    </div>
+            </thead>
+            <tbody>
+              {entrances.map((ent) => (
+                <tr
+                  key={ent.id}
+                  className="border-t border-gray-200 hover:bg-gray-50"
+                >
+                  <td className="px-4 py-2">{ent.id}</td>
+                  <td className="px-4 py-2">{ent.name}</td>
+                  <td className="px-4 py-2">{ent.location || "-"}</td>
+                  <td>
+                    {ent.users &&
+                      ent.users.map((user, i) => {
+                        if (i > 0)
+                          return <span key={user.id}>, {user.email}</span>;
+                        return <span key={user.id}>{user.email}</span>;
+                      })}
+                  </td>
+                  <td className="px-4 py-2 flex items-center ">
+                    <Link
+                      to={`/admin/entrances/${ent.id}/edit`}
+                      className="bg-blue-500 hover:bg-blue-600 text-white py-1 px-3 rounded text-xs cursor-pointer"
+                    >
+                      <Settings className="w-5 h-5" />
+                    </Link>
+                    <button className="bg-red-500 hover:bg-red-600 text-white py-1 px-3 rounded text-xs cursor-pointer ml-2">
+                      <Trash2 className="w-5 h-5" />
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div> */}
+      <EntrancesDataTable columns={columns} data={entrances} />
+    </>
   );
 };
 
---
+++ frontend/src/components/admin/EntranceTable/data-table.tsx
@@ -0,0 +1,117 @@
+import {
+  ColumnDef,
+  flexRender,
+  getCoreRowModel,
+  useReactTable,
+  getPaginationRowModel,
+} from "@tanstack/react-table";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { Button } from "@/components/ui/button";
+import { useState } from "react";
+import { Trash } from "lucide-react";
+
+interface DataTableProps<TData, TValue> {
+  columns: ColumnDef<TData, TValue>[];
+  data: TData[];
+}
+
+export function EntrancesDataTable<TData, TValue>({
+  columns,
+  data,
+}: DataTableProps<TData, TValue>) {
+  const [rowSelection, setRowSelection] = useState({});
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    onRowSelectionChange: setRowSelection,
+    state: {
+      rowSelection,
+    },
+  });
+
+  return (
+    <div>
+      <div className="rounded-md border">
+        <Table>
+          <TableHeader className="bg-white">
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map((header) => (
+                  <TableHead key={header.id}>
+                    {header.isPlaceholder
+                      ? null
+                      : flexRender(
+                          header.column.columnDef.header,
+                          header.getContext()
+                        )}
+                  </TableHead>
+                ))}
+              </TableRow>
+            ))}
+          </TableHeader>
+          <TableBody>
+            {table.getRowModel().rows.length ? (
+              table.getRowModel().rows.map((row) => (
+                <TableRow key={row.id}>
+                  {row.getVisibleCells().map((cell) => (
+                    <TableCell key={cell.id}>
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext()
+                      )}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell
+                  colSpan={columns.length}
+                  className="h-24 text-center"
+                >
+                  Žádná data.
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </div>
+      <div className="flex items-center justify-end space-x-2 py-4">
+        {/* <Button
+          onClick={() =>
+            console.log(
+              table.getSelectedRowModel().rows.map((row) => row.original)
+            )
+          }
+        >
+          selected
+        </Button> */}
+        <Button
+          variant="outline"
+          size="sm"
+          onClick={() => table.previousPage()}
+          disabled={!table.getCanPreviousPage()}
+        >
+          Předchozí
+        </Button>
+        <Button
+          variant="outline"
+          size="sm"
+          onClick={() => table.nextPage()}
+          disabled={!table.getCanNextPage()}
+        >
+          Další
+        </Button>
+      </div>
+    </div>
+  );
+}
