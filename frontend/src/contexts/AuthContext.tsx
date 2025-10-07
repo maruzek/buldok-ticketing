9e7501cfc4d54341f27c65de0564326f322bfb46
Martin Růžek <me@martinruzek.cz>
2025-10-07 11:27:02 +0200

    fixed wrong logout route

--- frontend/src/contexts/AuthContext.tsx
+++ frontend/src/contexts/AuthContext.tsx
@@ -61,7 +61,7 @@ const AuthProvider = ({ children }: AuthProviderProps) => {
 
     try {
       const response = await fetch(
-        `${import.meta.env.VITE_API_URL}/api/auth/logout`,
+        `${import.meta.env.VITE_API_URL}/auth/logout`,
         {
           method: "POST",
           headers: {
