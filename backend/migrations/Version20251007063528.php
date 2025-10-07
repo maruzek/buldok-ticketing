33af21971f13be78ecf2e302edb50858defb509b
Martin Růžek <me@martinruzek.cz>
2025-10-07 10:33:44 +0200

    migration fix

---
+++ backend/migrations/Version20251007063528.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Creates the 'season' table and adds a unique index.
+ */
+final class Version20251007063528 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Fix for messedup migration, creates the season table';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE season (id INT AUTO_INCREMENT NOT NULL, years VARCHAR(9) NOT NULL, start_at DATE NOT NULL, end_at DATE NOT NULL, created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', status VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_F0E45BA9A308E877 (years), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE season');
+    }
+}
